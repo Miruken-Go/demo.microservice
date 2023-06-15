fb05a9c4f6d34eeca322bd6d9bb3e39e2b24ae51
Michael  Dudley <provenstyle@gmail.com>
2023-06-15 17:10:02 -0500

    building and pushing the docker image

--- build-teamsrv.sh
+++ build-teamsrv.sh
@@ -1,12 +1,10 @@
-docker run -v $(pwd):/go/src --workdir=/go/src/teamsrv golang:1.20 go test ./...
-if [[ $? -gt 0 ]]; then 
-  echo "Failed to test"; 
-  exit 1; 
-fi
-
 NAME=teamsrv
 VERSION="v$(date +%s)"
 IMAGE_NAME="${NAME}shared.azurecr.io/${NAME}:$VERSION"; echo $IMAGE_NAME
+
+echo "VERSION: $VERSION"
+echo "IMAGE_NAME: $IMAGE_NAME"
+
 docker build --build-arg application_version=$VERSION -t $IMAGE_NAME $NAME
 if [[ $? -gt 0 ]]; then 
   echo "Failed to build docker image"; 
@@ -22,11 +20,12 @@ if [[ $? -gt 0 ]]; then
 fi
 
 TAG="$NAME/$VERSION"
+echo "TAG: $TAG"
 if [[ $(git tag -l "$TAG") ]];
     then
         echo "Tag already created"
     else
         echo "Tagging the release"
         git -c "user.name=buildpipeline" -c "user.email=mirukenjs@gmail.com" tag -a $TAG -m "Tagged by build pipeline"
-        # git -c "user.name=buildpipeline" -c "user.email=mirukenjs@gmail.com" push origin $TAG
+        git -c "user.name=buildpipeline" -c "user.email=mirukenjs@gmail.com" push origin $TAG
 fi;
