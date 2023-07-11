9ab09f6e7ba3d1eda715854f890af39105d5862c
Michael  Dudley <provenstyle@gmail.com>
2023-07-10 19:04:48 -0500

    make directory as safe

--- deployment/src/git.js
+++ deployment/src/git.js
@@ -15,6 +15,7 @@ async function tagAndPush(tag) {
             git config --global url."https://api:$ghToken@github.com/".insteadOf "https://github.com/"
             git config --global url."https://ssh:$ghToken@github.com/".insteadOf "ssh://git@github.com/"
             git config --global url."https://git:$ghToken@github.com/".insteadOf "git@github.com:"
+            git config --global --add safe.directory $(pwd)
             git -c "user.name=buildpipeline" -c "user.email=mirukenjs@gmail.com" tag -a ${tag} -m "Tagged by build pipeline"
             git -c "user.name=buildpipeline" -c "user.email=mirukenjs@gmail.com" push origin ${tag}
         `)
