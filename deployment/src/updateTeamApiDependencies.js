2b1ba9d55221ba7b5807d958220dd5e1f1713cb1
Michael  Dudley <provenstyle@gmail.com>
2023-07-13 15:48:10 -0500

    using go in the build image instead of a docker container call

--- deployment/src/updateTeamApiDependencies.js
+++ deployment/src/updateTeamApiDependencies.js
@@ -1,4 +1,3 @@
-const az      = require('./az');
 const bash    = require('./bash')
 const logging = require('./logging');
 const config  = require('./config');
@@ -14,19 +13,18 @@ async function main() {
         logging.header("Updating teamapi dependencies")
 
         await bash.execute(`
-            docker run --rm -v $(pwd):$(pwd) --workdir=$(pwd)/teamapi golang:1.20 pwd
+            cd teamapi
+            go get github.com/miruken-go/miruken@${config.mirukenVersion}
         `)
-        
-            //docker run --rm -v $(pwd):/go/src --workdir=/go/src/teamapi golang:1.20 go get github.com/miruken-go/miruken@${config.mirukenVersion}
 
-        //await git.commitAll(`Updated miruken to ${config.mirukenVersion}`)
-        //await git.push();
+        await git.commitAll(`Updated miruken to ${config.mirukenVersion}`)
+        await git.push();
 
-        // await bash.execute(`
-        //     gh workflow run update-team-dependencies.yml \
-        //         -f mirukenVersion=${mirukenVersion}      \
-        //         -f teamapiVersion=${version}             \
-        // `)
+        await bash.execute(`
+            gh workflow run update-team-dependencies.yml \
+                -f mirukenVersion=${mirukenVersion}      \
+                -f teamapiVersion=${version}             \
+        `)
 
         console.log("Updated teamapi dependencies")
     } catch (error) {
