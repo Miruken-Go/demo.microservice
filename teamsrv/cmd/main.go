a0ba1118079197664a097ba3200a70f1da8a6165
Michael  Dudley <provenstyle@gmail.com>
2023-09-19 17:21:00 -0500

    renaming to scopes

--- teamsrv/cmd/main.go
+++ teamsrv/cmd/main.go
@@ -28,10 +28,10 @@ import (
 
 func authzHandler(w http.ResponseWriter, r *http.Request) {
 	type Response struct {
-		Foo []string
+		Scopes []string
 	}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Response {Foo: []string{"Bar", "Baz"}})
+	json.NewEncoder(w).Encode(Response {Scopes: []string{"creatTeam", "updateTeam", "createPerson", "updatePerson"}})
 }
 
 func main() {
