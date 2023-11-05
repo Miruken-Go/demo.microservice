a7a92d486069f259e5e7a98b43c6ca402479c0ab
Michael  Dudley <provenstyle@gmail.com>
2023-11-05 09:52:27 -0600

    cleaning up config

--- deployment/src/config.js
+++ deployment/src/config.js
@@ -1,49 +1,62 @@
 const env = process.env.env
 if (!env) throw "Environment variable required: [env]"
 
-const appName         = 'teamsrv'.toLowerCase() //must be lowercase
-const repository      = 'https://github.com/Miruken-Go/demo.microservice'
-const defaultLocation = 'CentralUs'
+const appDescription ={
+    appName:    'teamsrv',
+    repository: 'https://github.com/Miruken-Go/demo.microservice',
+    location:   'CentralUs',
+    environments: [
+        'dev',
+        'qa',
+        'uat',
+        'demo',
+        'prod',
+        'dr'
+    ]
+}
 
-const instance = process.env.instance
-const prefix   = (instance) 
+const appName      = appDescription.appName.toLowerCase() //must be lowercase
+const repository   = appDescription.repository
+const location     = appDescription.location
+const globalPrefix = `${appName}-global`
+const commonPrefix = `${appName}-${env}`
+const instance     = process.env.instance
+
+const prefix = (instance) 
     ? `${appName}-${env}-${instance}`
     : `${appName}-${env}`
 
-const commonPrefix = `${appName}-${env}`
-
-const simplePrefix = prefix.replace(/[^A-Za-z0-9]/g, "").toLowerCase()
 const containerRepositoryName = `${appName}global`.replace(/[^A-Za-z0-9]/g, "").toLowerCase()
 
 if (containerRepositoryName.length > 32)
     throw `Configuration Error - containerRepositoryName cannot be longer than 32 characters : ${containerRepositoryName} [${containerRepositoryName.length}]`
 
-if (simplePrefix.length > 32)
-    throw `Configuration Error - simplePrefix cannot be longer than 50 characters because of ACA naming restrictions: ${simplePrefix} [${simplePrefix.length}]`
-
-const imageName = `${containerRepositoryName}.azurecr.io/${appName}`
+const imageName    = `${containerRepositoryName}.azurecr.io/${appName}`
 
 const keyVaultName = `${prefix}-keyvault` 
 
-const envSpecific = require(`./${env}.js`)
+//If the env environement varialbe is set to an expected environment
+//pull in environemnt specific configurations
+const envSpecific = (appDescription.environments.includes(env))
+    ? require(`./${env}.js`)
+    : {}
 
 const config = {
-    workingDirectory: process.cwd(),
-    nodeDirectory:    __dirname,
     env,
     instance,
     appName,
-    defaultContainerImage: 'defaultContainerImage',
     prefix,
-    simplePrefix,
-    globalResourceGroup: `${appName}-global`,
-    commonEnvironmentResourceGroup: `${commonPrefix}-common`,
-    environmentInstanceResourceGroup: `${prefix}`,
     containerRepositoryName,
     imageName,
     keyVaultName,
-    location: process.env.location || defaultLocation,
+    location,
     repository,
+    workingDirectory:                 process.cwd(),
+    nodeDirectory:                    __dirname,
+    defaultContainerImage:            'defaultContainerImage',
+    globalResourceGroup:              globalPrefix,
+    commonEnvironmentResourceGroup:   `${commonPrefix}-common`,
+    environmentInstanceResourceGroup: `${prefix}`,
     secrets: {},
     ...envSpecific,
     requiredSecrets: function (names) {
--- deployment/src/global.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    b2cDeploymentPipelineClientId:            'na',
-    identityExperienceFrameworkClientId:      'na',
-    proxyIdentityExperienceFrameworkClientId: 'na',
-    b2cDomainName:                            'na',
-    authorizationServiceUrl:                   'na',
-}
