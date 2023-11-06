7e8051b3b1bce35f533385576e597614184fd0cc
Michael  Dudley <provenstyle@gmail.com>
2023-11-06 17:39:48 -0600

    updating the dev config

--- deployment/src/dev.js
+++ deployment/src/dev.js
@@ -4,6 +4,6 @@ module.exports = {
     proxyIdentityExperienceFrameworkClientId: "19b19c1e-618c-4c22-a6ab-c08e098d69fe",
     b2cDomainName:                            "teamsrvidentitydev.onmicrosoft.com",
     wellKnownOpenIdConfigurationUrl:          "https://teamsrvidentitydev.b2clogin.com/teamsrvidentitydev.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_SIGNUP_SIGNIN",
-    authorizationServiceUrl:                  "https://teamsrv-dev-ci.livelyisland-03fdcde0.centralus.azurecontainerapps.io/authz/",
-    authorizationServiceUsername:             "ooYymDzee5!V&v8gk7*s",
+    authorizationServiceUrl:                  "https://teamsrv-dev.prouddune-3b6fb895.centralus.azurecontainerapps.io/authz/",
+    authorizationServiceUserName:             "ooYymDzee5!V&v8gk7*s",
 }
