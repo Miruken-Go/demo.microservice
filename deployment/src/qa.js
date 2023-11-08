8388528c1cb6c8f17b0235789beee7a0eb854ed3
Michael  Dudley <provenstyle@gmail.com>
2023-11-07 19:07:07 -0600

    documenting manual steps

--- deployment/src/qa.js
+++ deployment/src/qa.js
@@ -1,4 +1,6 @@
 module.exports = {
-    b2cDomainName:                 "teamsrvidentityqa.onmicrosoft.com",
-    b2cDeploymentPipelineClientId: "2c51617b-8287-4b69-a2b5-2a499b53bd60"
+    b2cDomainName:                            "teamsrvidentityqa.onmicrosoft.com",
+    b2cDeploymentPipelineClientId:            "2c51617b-8287-4b69-a2b5-2a499b53bd60",
+    identityExperienceFrameworkClientId:      "e73da011-9782-4294-99c6-438e946371e5",
+    proxyIdentityExperienceFrameworkClientId: "e1d1475e-6339-41ea-af75-85ed730785bf",
 }
