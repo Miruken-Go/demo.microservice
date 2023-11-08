0f0db221f70e6c853c864404d1cfb42b4fd4fdf6
Michael  Dudley <provenstyle@gmail.com>
2023-11-07 18:16:19 -0600

    updating manual-steps

--- deployment/src/dev.js
+++ deployment/src/dev.js
@@ -1,8 +1,8 @@
 module.exports = {
+    b2cDomainName:                            "teamsrvidentitydev.onmicrosoft.com",
     b2cDeploymentPipelineClientId:            "a585c10f-7a10-453a-9a09-0305cc6af1ed",
     identityExperienceFrameworkClientId:      "efa7c5ab-b77b-4a0b-933e-974d528dac20",
     proxyIdentityExperienceFrameworkClientId: "19b19c1e-618c-4c22-a6ab-c08e098d69fe",
-    b2cDomainName:                            "teamsrvidentitydev.onmicrosoft.com",
     wellKnownOpenIdConfigurationUrl:          "https://teamsrvidentitydev.b2clogin.com/teamsrvidentitydev.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_SIGNUP_SIGNIN",
     authorizationServiceUrl:                  "https://teamsrv-dev.prouddune-3b6fb895.centralus.azurecontainerapps.io/authz/",
     authorizationServiceUsername:             "ooYymDzee5!V&v8gk7*s",
--- deployment/src/qa.js
+++ deployment/src/qa.js
@@ -1,3 +1,4 @@
 module.exports = {
-    
+    b2cDomainName:                 "teamsrvidentityqa.onmicrosoft.com",
+    b2cDeploymentPipelineClientId: "2c51617b-8287-4b69-a2b5-2a499b53bd60"
 }
