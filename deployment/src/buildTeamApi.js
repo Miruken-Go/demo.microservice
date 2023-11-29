c19ed1b15432cd61117f9293d794b1befbe46e3f
Michael  Dudley <provenstyle@gmail.com>
2023-11-28 21:44:33 -0600

    whitespace

--- deployment/src/buildTeamApi.js
+++ deployment/src/buildTeamApi.js
@@ -24,13 +24,16 @@ async function main() {
             cd ../
         `)
 
+        //This docker container is running docker in docker from github actions
+        //Therefore using $(pwd) to get the working directory would be the working directory of the running container 
+        //Not the working directory from the host system. So we need to pass in the repository path.
         const rawVersion = await bash.execute(`
-            docker run                                     / 
-                --rm                                       / 
-                -v "${variables.repositoryPath}:/repo"        / 
+            docker run                                     /
+                --rm                                       /
+                -v "${variables.repositoryPath}:/repo"     /
                 gittools/gitversion:5.12.0-alpine.3.14-6.0 /
-                    /repo                                  / 
-                    /showvariable SemVer                   / 
+                    /repo                                  /
+                    /showvariable SemVer                   /
                     /overrideconfig tag-prefix=team-api/v  /
         `)
 
