e71f956258ef4a844fa9efbb16a7f50ff581f140
Michael  Dudley <provenstyle@gmail.com>
2023-12-01 08:16:13 -0600

    moving infrastructure to a submodule

--- deployment/src/infrastructure/go.js
+++
@@ -1,8 +0,0 @@
-import * as bash from './bash.js'
-
-export async function getModuleVersion(folder, module) { 
-    return await bash.execute(`
-        cd ${folder}
-        go list -m all | grep ${module} | awk '{print $2}' \
-    `)
-}
