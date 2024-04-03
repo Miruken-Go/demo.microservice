7bfe8b6fea3398bcb767c0b17939163f6b8c7eba
michael dudley <michael.dudley@improving.com>
2024-04-03 11:55:35 -0500

    updating the build container version

--- major-league-miruken/cicd/jest.config.ts
+++ major-league-miruken/cicd/jest.config.ts
@@ -2,6 +2,9 @@ import type { Config } from 'jest'
 
 const config: Config = {
   verbose: true,
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
   testPathIgnorePatterns: ['/node_modules/', '/dist/']
 }
 
