e3226c0e69e431b76ae557106516c6449ebe4abe
michael dudley <michael.dudley@improving.com>
2024-04-03 22:14:21 -0500

    adding cicd to team

---
+++ team/cicd/src/publish.ts
@@ -0,0 +1,66 @@
+import { 
+    handle,
+    logging,
+    bash,
+    EnvSecrets,
+    EnvVariables,
+    go,
+    Git,
+    GH
+} from 'ci.cd'
+
+handle(async() => {
+    const variables = new EnvVariables()
+        .required([
+            'repositoryPath',
+            'repository',
+            'repositoryOwner',
+            'ref',
+        ])
+        .optional(['skipRepositoryDispatches'])
+        .variables
+    logging.printVariables(variables)
+
+    const secrets = new EnvSecrets()
+        .require(['GH_TOKEN'])
+        .secrets
+    logging.printSecrets(secrets)
+
+    logging.header("Publishing team")
+
+    await bash.execute(`
+        cd ../
+        go test ./...
+    `)
+
+    //This docker container is running docker in docker from github actions
+    //Therefore using $(pwd) to get the working directory would be the working directory of the running container 
+    //Not the working directory from the host system. So we need to pass in the repository path.
+    const rawVersion = await bash.execute(`
+        docker run --rm -v '${variables.repositoryPath}:/repo' \
+        gittools/gitversion:5.12.0-alpine.3.14-6.0 /repo /showvariable SemVer /overrideconfig tag-prefix=team/v
+    `)
+
+    const version = `v${rawVersion}`
+    const tag     = `team/${version}`
+
+    console.log(`version: [${version}]`)
+    console.log(`tag:     [${tag}]`)
+
+    await new Git(secrets.GH_TOKEN)
+        .tagAndPush(tag)
+
+    const mirukenVersion = await go.getModuleVersion('../', 'github.com/miruken-go/miruken')
+    console.log(`mirukenVersion: [${mirukenVersion}]`)
+    
+    await new GH({
+        ghToken:                  secrets.GH_TOKEN,
+        ref:                      variables.ref,
+        repository:               variables.repository,
+        repositoryOwner:          variables.repositoryOwner,
+        skipRepositoryDispatches: Boolean(variables.skipRepositoryDispatches)
+    }).sendRepositoryDispatch('built-team', {
+        mirukenVersion: mirukenVersion,
+        teamapiVersion: version,
+    }, variables.repository)
+})
